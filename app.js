08d5b5bd16c4f1d77e6113705869bd1cde4cd776
Valdir-J <algumascoisasaleatorias@gmail.com>
2025-08-09 14:26:57 -0300

    Adicionar lógica para gerenciar e sortear amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+const input = document.querySelector("#amigo");
+const listaAmigos = document.querySelector("#listaAmigos");
+const resultado = document.querySelector("#resultado");
+
+const adicionarAmigo = () => {
+  const nomeDoAmigo = input.value.trim();
+
+  if (nomeDoAmigo) {
+    amigos.push(nomeDoAmigo);
+    atualizarAmigos();
+    input.value = "";
+  } else {
+    alert("Por favor, insira um nome válido.");
+  }
+};
+
+const atualizarAmigos = () => {
+  listaAmigos.innerHTML = "";
+  resultado.textContent = "";
+
+  amigos.forEach((amigo) => {
+    const lista = document.createElement("li");
+    lista.textContent = amigo;
+    listaAmigos.appendChild(lista);
+  });
+};
+
+const sortearAmigo = () => {
+  if (amigos.length === 0) {
+    alert("Por favor, insira pelo menos um amigo na lista.");
+    return;
+  }
+
+  listaAmigos.innerHTML = "";
+
+  const numeroAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSecreto = amigos[numeroAleatorio];
+
+  resultado.textContent = amigoSecreto;
+};
